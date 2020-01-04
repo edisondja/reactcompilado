dd82a50da9137f08002704816a9ef61a76d5e12f
=MisterED <=edisondja@gmail.com>
2020-01-04 15:23:38 -0400

    Primeros cambios

---
+++ precache-manifest.f750f630d901a970cdbb9d07da7cb43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c10cc559089f7e063a583c743014cb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65f9d7a79085c9628cb",
+    "url": "/static/css/main.3324464f.chunk.css"
+  },
+  {
+    "revision": "db42acd61e77a4fdb5ba",
+    "url": "/static/js/2.0db7908d.chunk.js"
+  },
+  {
+    "revision": "d65f9d7a79085c9628cb",
+    "url": "/static/js/main.fde4b31b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a8cd92c5fa76b738b96a0ad89dc4ffc",
+    "url": "/static/media/logo.9a8cd92c.jpg"
+  }
+]);
